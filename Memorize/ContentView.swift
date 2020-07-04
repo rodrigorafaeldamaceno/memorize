87ca7428d3119ca4a29448f45cacfc503e1623a3
Rodrigo <rodrigorafaeldamaceno@gmail.com>
2020-07-04 09:42:29 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Rodrigo Rafael on 04/07/20.
+//  Copyright © 2020 Rodrigo Rafael. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
